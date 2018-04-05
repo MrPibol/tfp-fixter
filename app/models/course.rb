883e16bcd900ded59acd7e199335b5f7272af984
MrPibol <mr.jp.torres@gmail.com>
2018-04-05 21:18:08 +0000

    3. CRUD For Our App. 17: Sections - Migration

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
     belongs_to :user
+    has_many :sections
     validates :title, :description, presence: { message: "must be given please"}
     validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0, message: "please provide a valid number"}
 end
