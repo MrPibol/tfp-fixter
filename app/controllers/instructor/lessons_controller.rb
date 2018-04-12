37a94aa1deddea62833578dc0e49b73b37d52e6a
MrPibol <mr.jp.torres@gmail.com>
2018-04-12 18:41:04 +0000

    3. CRUD For Our App. 19:Lesson Permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,21 +1,39 @@
 class Instructor::LessonsController < ApplicationController
+    before_action :authenticate_user!
     def new
-        @section = Section.find(params[:section_id])
+        @section = current_section
+        if @section.course.user != current_user
+            return render plain: "Unauthorized", status: :unauthorized
+        end
         @lesson = Lesson.new
     end
     
     def create
-        @section = Section.find(params[:section_id])
-        @lesson = @section.lessons.create(lesson_params)
+        @section = current_section
         
-        if @lesson.valid?
-            redirect_to instructor_course_path(@section.course)
+        if @section.course.user != current_user
+            return render plain: "Unauthorized", status: :unauthorized
         else
-            render :new, status: :unprocessable_entity 
+            @lesson = @section.lessons.create(lesson_params)
+            if !@lesson.valid?
+                render :new, status: :unprocessable_entity 
+            end
+            redirect_to instructor_course_path(@section.course)
         end
     end
     
     private
+    def current_section
+        @current_section ||= Section.find(params[:section_id])
+        
+        # if @current_section == nil
+        #     @current_section = Section.find(params[:section_id])
+        #     @current_section
+        # else
+        #     @current_session
+        # end   ----> this section is replaced with @current_section ||= Section.find(params[:section_id])
+    end
+    
     def lesson_params
         params.require(:lesson).permit(:title, :subtitle)
     end
