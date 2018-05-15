061b00daef5c90c2113e18665b91125d1acd0563
MrPibol <mr.jp.torres@gmail.com>
2018-05-15 05:15:10 +0000

    5. Enrollment & Payments. 28 Challenge: Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_enrollment_for_viewing_lesson, only: [:show]
     def show
     end
     
@@ -6,5 +8,12 @@ class LessonsController < ApplicationController
     helper_method :current_lesson
     def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
+    end
+    
+    def require_enrollment_for_viewing_lesson
+        if !current_user.enrolled_in?(current_lesson.section.course)
+            flash[:notice] = "Enrollment to the course is required"
+            redirect_to course_path(current_lesson.section.course)
+        end
     end
 end
