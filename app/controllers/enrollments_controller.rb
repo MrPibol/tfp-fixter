a89a574caa836a796046c2801ffb7a95c5296ed9
MrPibol <mr.jp.torres@gmail.com>
2018-05-25 09:15:07 +0000

    courses/show.html.erb fixed - nested if

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,24 +1,24 @@
 class EnrollmentsController < ApplicationController
     def create
+      
+      if current_course.premium?
+         # Amount in cents
+         @amount = (current_course.cost*100).to_i
+      
+          customer = Stripe::Customer.create(
+            email: params[:stripeEmail],
+            source: params[:stripeToken]
+          )
+      
+          charge = Stripe::Charge.create(
+            customer: customer.id,
+            amount: @amount,
+            description: 'Fixter Premium Content',
+            currency: 'usd'
+          )
+      end
       @enrollment = current_user.enrollments.create(course: current_course)
-       
-       # Amount in cents
-       @amount = (current_course.cost*100).to_i
-    
-        customer = Stripe::Customer.create(
-          email: params[:stripeEmail],
-          source: params[:stripeToken]
-        )
-    
-        charge = Stripe::Charge.create(
-          customer: customer.id,
-          amount: @amount,
-          description: 'Fixter Premium Content',
-          currency: 'usd'
-        )
-
-
-        redirect_to course_path(current_course)
+      redirect_to course_path(current_course)
     
         rescue Stripe::CardError => e
           flash[:error] = e.message
